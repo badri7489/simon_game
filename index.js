b50c246684e84c6b22c4282ea1030b1a6d8bead6
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-04 20:14:00 +0530

    Styles and Html Done

---
+++ index.js
@@ -0,0 +1,9 @@
+// function randomButton() {
+// 	var button = Math.floor(Math.random() * 4) + 1;
+// }
+
+// var i = 1;
+// var game_over = false;
+// while (!game_over) {
+// 	$("h1").text("Level " + i);
+// }
